660666ec674b1203db1012dd4a6222e4329081ea
Fanely <55720797+alunaufginf@users.noreply.github.com>
2021-02-23 21:23:24 -0500

    Add files via upload
    seeders

---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // password
+        User::factory(['email' => 'admin@tiendavirtual.com', 'name' => 'Administrador del Sistema'])->create();
+    }
+}
