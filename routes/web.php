38815e25e95b7fac1ccd41c4d9bb0f5f9d507bd0
Fanely <55720797+alunaufginf@users.noreply.github.com>
2021-02-23 21:25:12 -0500

    Add files via upload
    routes

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,61 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ColorController;
+use App\Http\Controllers\EnvioController;
+use App\Http\Controllers\GeneroController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\MonedaController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\PerfilController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\SubcategoriaController;
+use App\Http\Controllers\TipoDocumentoController;
+use App\Http\Controllers\TipoPagoController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio');
 
-Route::get("/admin", [AdminController::class, 'inicio']);
-Route::get("/usuarios", [AdminController::class, 'usuarios']);
+Route::middleware('auth')->group(function () {
+    Route::get("/admin", [AdminController::class, 'inicio'])->name('admin');
+
+    // Productos
+    Route::get('/categoria/search', [CategoriaController::class, 'buscar'])->name('categoria.buscar');
+    Route::resource('/categoria', CategoriaController::class);
+    Route::get('/subcategoria/search', [SubcategoriaController::class, 'buscar'])->name('subcategoria.buscar');
+    Route::resource('/subcategoria', SubcategoriaController::class);
+    Route::get('/marca/search', [MarcaController::class, 'buscar'])->name('marca.buscar');
+    Route::resource('/marca', MarcaController::class);
+    Route::get('/producto/search', [ProductoController::class, 'buscar'])->name('producto.buscar');
+    Route::resource('/producto', ProductoController::class);
+    Route::resource('/color', ColorController::class);
+    Route::resource('/genero', GeneroController::class);
+
+    // Cliente
+    Route::get('/cliente/search', [ClienteController::class, 'buscar'])->name('cliente.buscar');
+    Route::resource('/cliente', ClienteController::class);
+
+
+    // Ventas
+    Route::resource('/pedido', PedidoController::class);
+    Route::resource('/envio', EnvioController::class);
+
+    // Configuracion
+    Route::resource('/moneda', MonedaController::class);
+    Route::resource('/tipo_pago', TipoPagoController::class);
+    Route::resource('/tipo_documento', TipoDocumentoController::class);
+
+    // Usuarios
+    Route::get('/usuario/search', [UsuarioController::class, 'buscar'])->name('usuario.buscar');
+    Route::resource('/usuario', UsuarioController::class);
+    Route::resource('/rol', RolController::class);
+
+    // Perfil
+    Route::get('/perfil', [PerfilController::class, 'index'])->name('perfil');
+});
